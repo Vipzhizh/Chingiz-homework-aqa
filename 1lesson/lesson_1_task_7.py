bc8f786ef4150c8d32a4c054c115545bfdcd487f
Vipzhizh <vipchizh@mail.ru>
2023-03-01 01:14:36 +0500

    обновление

---
+++ 1lesson/lesson_1_task_7.py
@@ -0,0 +1,39 @@
+def a():
+    print (1)
+def b():
+    print (2)
+
+def c():
+    print (3)
+
+def d():
+    print (4)
+
+
+def e():
+    print(5)
+
+def f():
+    print(6)
+
+def g():
+    print(7)
+
+def h():
+    print(8)
+
+def i():
+    print(9)
+
+def j():
+    print(0)
+
+h(), h(), j()
+j()
+e()
+e()
+e()
+c()
+e()
+c()
+e()
---
+++ 1lesson/lesson_1_task_8.py
@@ -0,0 +1,14 @@
+def any(num):
+    print(num)
+any(8)
+any(8)
+any(0)
+any(0)
+any(5)
+any(5)
+any(5)
+any(3)
+any(5)
+any(3)
+any(5)
+ 
